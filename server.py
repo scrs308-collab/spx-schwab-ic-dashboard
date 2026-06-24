52a3551afde5f79e7dc26ddf10f6e97d69dd2fb3
joebutler1966 <144394665+joebutler1966@users.noreply.github.com>
2026-06-23 21:37:37 -0400

    Fix SPX symbol conversion

--- server.py
+++ server.py
@@ -178,7 +178,7 @@ def api_recommend(
     count: int = Query(10)
 ):
     if symbol.upper() == "SPX":
-    symbol = "$SPX"
-    
+        symbol = "$SPX"
+
     chain = get_option_chain(symbol=symbol, strike_count=strike_count)
     return recommend_ics(chain, dte=dte, wing_width=wing_width, min_credit=min_credit, max_spread=max_spread, count=count)
